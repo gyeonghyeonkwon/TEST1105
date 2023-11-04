ab1e55f334467e2b03418b9dc8a9bd21591c4e50
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-11-05 00:13:41 +0900

    1단계테스트

---
+++ src/main/java/com/ll/Main.java
@@ -0,0 +1,14 @@
+package com.ll;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.SortedMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("명령 : ");
+        String sc = scanner.nextLine();
+    }
+}
