857695694b5c33533933b0b488a9094cc52aa941
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-11-05 03:23:40 +0900

    6단계삭제?id=1 완성

---
+++ src/main/java/com/ll/Inventory.java
@@ -0,0 +1,12 @@
+package com.ll;
+
+public class Inventory {
+    String wiseSaying;
+    String another;
+    int id;
+    Inventory(String wiseSaying,String another,int id){
+        this.wiseSaying=wiseSaying;
+        this.another=another;
+        this.id=id;
+    }
+}
--- src/main/java/com/ll/app.java
+++ src/main/java/com/ll/app.java
@@ -1,27 +1,68 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class app {
+    Scanner scanner = new Scanner(System.in);
+    int wiseSaingNumber = 0;
+    List<Inventory> inventoryList = new ArrayList<>();
+
     public void run() {
-        Scanner scanner = new Scanner(System.in);
-        int wiseSaingNumber = 0;
+
         while (true) {
             System.out.print("명령 : ");
             String sc = scanner.nextLine();
-            if(sc.equals("종료")){
+            if (sc.equals("종료")) {
                 break;
-            }
-            else if (sc.equals("등록")){
-                System.out.print("명언 : ");
-                String wiseSaying = scanner.nextLine();
-                System.out.print("작가 : ");
-                String another = scanner.nextLine();
-                wiseSaingNumber ++;
-                System.out.printf("%d번 명언이 등록되었습니다%n",wiseSaingNumber);
+            } else if (sc.equals("등록")) {
+                registration();
+            } else if (sc.equals("목록")) {
+                todoList();
+            } else if (sc.startsWith("삭제?")) {
+                todoListRemove(sc);
             }
         }
     }
-}
 
+    void registration() {
+        System.out.print("명언 : ");
+        String wiseSaying = scanner.nextLine();
+        System.out.print("작가 : ");
+        String another = scanner.nextLine();
+        wiseSaingNumber++;
+        int id = wiseSaingNumber;
+        System.out.printf("%d번 명언이 등록되었습니다%n", id);
+        Inventory inventory = new Inventory(wiseSaying, another, id);
+        inventoryList.add(inventory);
+    }
+
+    void todoList() {
+        System.out.println("번호 / 작가 /명언");
+        System.out.println("-".repeat(18));
+        for (int i = inventoryList.size() - 1; i >= 0; i--) {
+            Inventory inventory = inventoryList.get(i);
+            System.out.printf("%d %s %s %n", inventory.id, inventory.another, inventory.wiseSaying);
+        }
+    }
+
+    void todoListRemove(String sc) { //삭제?id=1
+        String [] scBit = sc.split("\\?", 2);
+        String action = scBit[0];
+        String queryString = scBit[1];
+        String [] queryStringBit= queryString.split("&");
+        int id =0;
+        for(int i=0; i < queryStringBit.length; i++){ // id=1
+            String queryStringStrBit = queryStringBit[i];
+            String [] queryStringStrBits= queryStringStrBit.split("=",2); //"id" , "2"
+            String paramName = queryStringStrBits[0]; //"id"
+            String paramValue = queryStringStrBits[1]; //"2"
+            if (paramName.equals("id")){
+                id = Integer.parseInt(paramValue);
+            }
+            System.out.printf("%d번 명언을 삭제합니다 %n",id);
+        }
+    }
+}
 
